3c725b69c03c56c70632b460fc1accb3c2644441
sue <sue@srcginc.com>
2017-07-13 12:51:08 -0700

    Set up Mongoose. See deprecation warning generated in Webstorm on server run in desc
    (node:32649) DeprecationWarning: `open()` is deprecated in mongoose >=
    4.11.0, use `openUri()` instead, or set the `useMongoClient` option if
    using `connect()` or `createConnection()`. See
    http://mongoosejs.com/docs/connections.html#use-mongo-client

--- blog/mean-blog/server.js
+++ blog/mean-blog/server.js
@@ -6,6 +6,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blogappignition');
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
